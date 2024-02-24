66ed404ae8af29f0340cb5b7646f1738e7360029
otavio-silva-jua <otavio.silva@sabaojua.com.br>
2024-02-24 16:14:43 -0300

    Banco de dados

--- querys.sql
+++ querys.sql
@@ -1,25 +1,138 @@
-create database mvc;
-use mvc;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 24-Fev-2024 às 20:13
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-create table tb_produtos (
-	id int not null primary key auto_increment,
-	descricao varchar(200) not null,
-	preco float(8,2) not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-insert into tb_produtos(descricao, preco)values('Sofá', 1250.75);
-insert into tb_produtos(descricao, preco)values('Cadeira', 378.99);
-insert into tb_produtos(descricao, preco)values('Cama', 870.75);
-insert into tb_produtos(descricao, preco)values('Notebook', 1752.49);
-insert into tb_produtos(descricao, preco)values('Smartphone', 999.99);
+--
+-- Banco de dados: `mvc`
+--
+CREATE DATABASE IF NOT EXISTS `mvc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mvc`;
 
-create table tb_info (
-	id int not null primary key auto_increment,
-	titulo varchar(200) not null,
-	descricao text not null
-);
+-- --------------------------------------------------------
 
-insert into tb_info(titulo, descricao)values('Missão', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-insert into tb_info(titulo, descricao)values('Visão', 'Morbi faucibus elit nec nibh elementum, a ultrices ante condimentum.');
-insert into tb_info(titulo, descricao)values('Valores', 'Pellentesque faucibus egestas justo sed malesuada. Etiam convallis tortor diam, scelerisque sodales nibh consequat quis. Nullam sodales nunc neque, eu placerat ex ultrices a. Nulla sed sapien eu orci egestas imperdiet fringilla ut massa.');
+--
+-- Estrutura da tabela `tb_info`
+--
+
+CREATE TABLE `tb_info` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(200) NOT NULL,
+  `descricao` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `tb_info`
+--
+
+INSERT INTO `tb_info` (`id`, `titulo`, `descricao`) VALUES
+(1, 'Missão', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+(2, 'Visão', 'Morbi faucibus elit nec nibh elementum, a ultrices ante condimentum.'),
+(3, 'Valores', 'Pellentesque faucibus egestas justo sed malesuada. Etiam convallis tortor diam, scelerisque sodales nibh consequat quis. Nullam sodales nunc neque, eu placerat ex ultrices a. Nulla sed sapien eu orci egestas imperdiet fringilla ut massa.');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_produtos`
+--
+
+CREATE TABLE `tb_produtos` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(200) NOT NULL,
+  `preco` float(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `tb_produtos`
+--
+
+INSERT INTO `tb_produtos` (`id`, `descricao`, `preco`) VALUES
+(1, 'O pequeno principe', 125.75),
+(2, 'O principe', 37.99),
+(3, 'Arte da guerra', 80.75),
+(4, 'Joao o pe de feijao', 52.49),
+(5, 'Vidas secas', 99.99);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `tipo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `senha`, `nome`, `tipo`) VALUES
+(1, 'admin@gmail.com', '123', 'otavio', 'adm');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tb_info`
+--
+ALTER TABLE `tb_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_info`
+--
+ALTER TABLE `tb_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
